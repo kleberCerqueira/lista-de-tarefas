2887683049418a7c87dc27b9f0cae945555fd533
Kleber <klebercerqueira.dev@gmail.com>
2025-11-25 13:53:06 -0300

    feat: completed static ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,137 @@
-const Home = () => {
-    return (
-        <div>teste</div>
-    )
-}
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { AlertDialogCancel } from "@radix-ui/react-alert-dialog";
+import {
+  Check,
+  CircleEllipsis,
+  List,
+  ListChecks,
+  Plus,
+  Sigma,
+  SquarePen,
+  Trash,
+} from "lucide-react";
 
-export default Home
+const Home = () => {
+  return (
+    <main className="bg-gray-100 w-full h-screen flex justify-center items-center">
+      <Card className="w-lg">
+        <CardHeader className="flex gap-2">
+          <Input placeholder="Adicionar tarefa" />
+          <Button className="cursor-pointer">
+            <Plus />
+            Adicionar
+          </Button>
+        </CardHeader>
+
+        <CardContent>
+          <Separator className="mb-4" />
+
+          <div className="flex gap-2">
+            <Badge className="cursor-pointer">
+              <List />
+              Todas
+            </Badge>
+            <Badge variant="outline" className="cursor-pointer">
+              <CircleEllipsis />
+              Não finalizadas
+            </Badge>
+            <Badge variant="outline" className="cursor-pointer">
+              <Check />
+              Concluídas
+            </Badge>
+          </div>
+
+          <div className="mt-4 border-b">
+            <div className="h-14 flex justify-between items-center border-t">
+              <div className="w-1 h-full bg-green-300"></div>
+              <p className="flex-1 px-2 text-sm">Estudar React</p>
+
+              <div className="flex gap-2 items-center">
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <SquarePen size={16} className="cursor-pointer" />
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Editar tarefa</DialogTitle>
+                    </DialogHeader>
+
+                    <div className="flex gap-2">
+                      <Input placeholder="Editar tarefa" />
+                      <Button className="cursor-pointer">Editar</Button>
+                    </div>
+                  </DialogContent>
+                </Dialog>
+                <Trash size={16} className="cursor-pointer" />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-between mt-4">
+            <div className="flex gap-2 items-center">
+              <ListChecks size={18} />
+              <p className="text-xs">Tarefas concluídas (3/3)</p>
+            </div>
+
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button
+                  className="text-xs h-7 cursor-pointer"
+                  variant="outline"
+                >
+                  <Trash />
+                  Limpar tarefas concluídas
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>
+                    Tem certeza que deseja excluir x itens?
+                  </AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogAction>Sim</AlertDialogAction>
+                  <AlertDialogCancel>Não</AlertDialogCancel>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+
+          <div className="h-2 w-full bg-gray-100 mt-4 rounded-md">
+            <div
+              className="h-full bg-blue-500 rounded-md"
+              style={{ width: "50%" }}
+            ></div>
+          </div>
+
+          <div className="flex justify-end items-center mt-2 gap-2">
+            <Sigma size={18} />
+            <p className="text-xs">3 tarefas no total</p>
+          </div>
+        </CardContent>
+      </Card>
+    </main>
+  );
+};
+
+export default Home;
